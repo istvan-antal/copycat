8dd0e6dafee0717a835fdf214d04cd749062913a
István Antal <istvan.m.antal@gmail.com>
2016-06-13 06:21:26 +0100

    Shortern long descriptions

--- views/ProgressScreen.jsx
+++ views/ProgressScreen.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+function formatDescription(description) {
+    if (description < 50) {
+        return description;
+    }
+    return '...' + description.slice(-50);
+}
+
 export const ProgressScreen = ({ progress }) => (
     <ul className="list-group">
         <li className="list-group-item">
-            { progress.description }
+            { formatDescription(progress.description) }
             <progress max="100" value={ progress.percentage || 0.1 }>
             </progress>
             { progress.speedText }
