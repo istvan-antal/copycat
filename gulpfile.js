e136ac4e0244ece4bfaf688f880da7177ab8ba49
István Antal <istvan.m.antal@gmail.com>
2016-06-15 06:29:32 +0100

    Added Webpack dev server

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const eslint = require('gulp-eslint');
 const fs = require('fs');
 
 gulp.task('build-js', () =>
-    gulp.src('app.jsx')
+    gulp.src('app.js')
         .pipe(webpackStream(webpackConfiguration))
         .pipe(gulp.dest('dist/'))
 );
@@ -17,4 +17,55 @@ gulp.task('check', () => {
     .pipe(eslint(config))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
+});
+
+gulp.task('index.html', () =>
+    gulp.src(__dirname + '/index.html')
+        .pipe(gulp.dest('dist/'))
+);
+
+gulp.task('photon', () =>
+    gulp.src(__dirname + '/node_modules/photon/**/*', { base: '.' })
+        .pipe(gulp.dest('dist/'))
+);
+
+gulp.task('runner-files', ['index.html', 'photon']);
+
+/* eslint no-console: 0 */
+const path = require('path');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+
+gulp.task('run', ['runner-files'], () => {
+    const config = Object.assign(
+        {},
+        webpackConfiguration,
+        {
+            output: {
+                filename: 'app.dev.js',
+                publicPath: 'dist',
+                path: path.resolve(__dirname, 'dist')
+            }
+        }
+    );
+    config.entry = {
+        'app.dev': './app.dev.js'
+    };
+
+    const compiler = webpack(config);
+
+    //console.log(config);
+
+    new WebpackDevServer(compiler, {
+        contentBase: path.resolve(__dirname, 'dist')
+    }).listen(8080, 'localhost', (err) => {
+        if (err) {
+            throw err;
+        }
+        // Server listening
+        console.log('[webpack-dev-server]', 'http://localhost:8080/webpack-dev-server/index.html');
+
+        // keep the server alive or continue?
+        // callback();
+    });
 });
