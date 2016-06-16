4be24b9d1ff2f330725bb447816d83574c224331
István Antal <istvan.m.antal@gmail.com>
2016-06-16 06:40:16 +0100

    Properly align progress bar with text

--- views/ProgressScreen.jsx
+++ views/ProgressScreen.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 
-function formatDescription(description) {
-    if (description < 50) {
-        return description;
-    }
-    return '...' + description.slice(-50);
-}
+import './ProgressScreen.css';
 
 export const ProgressScreen = ({ progress }) => (
-    <div style={ { padding: 10, textAlign: 'center' } }>
-        <div>
-            { formatDescription(progress.description) }
-        </div>
-        <div>
-            <progress max="100" value={ progress.percentage || 0.1 }/>
-        </div>
-        <div>
-            { progress.speedText }
+    <div className="outer">
+        <div className="middle">
+            <div className="inner">
+                <div>
+                    { progress.description }
+                </div>
+                <div>
+                    <progress max="100" value={ progress.percentage || 0.1 }/>
+                </div>
+                <div>
+                    { progress.speedText }
+                </div>
+            </div>
         </div>
     </div>
 );
