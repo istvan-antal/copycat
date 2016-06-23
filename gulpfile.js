477582ce5c1d3781d1050e2e3b8456d2ad728a2e
István Antal <istvan.m.antal@gmail.com>
2016-06-23 05:43:50 +0100

    Added package task

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,29 @@ const fs = require('fs');
 
 gulp.task('build-js', () =>
     gulp.src('app.js')
-        .pipe(webpackStream(webpackConfiguration))
-        .pipe(gulp.dest('dist/'))
+    .pipe(webpackStream(webpackConfiguration))
+    .pipe(gulp.dest('dist/'))
 );
 
 gulp.task('check', () => {
     const eslintrcPath = process.cwd() + '/.eslintrc.json';
     const config = JSON.parse(fs.readFileSync(eslintrcPath));
     return gulp.src(['*.jsx', '*.js', '**/*.js', '**/*.jsx', '!node_modules/**', '!dist/**'])
-    .pipe(eslint(config))
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+        .pipe(eslint(config))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('index.html', () =>
     gulp.src(__dirname + '/index.html')
-        .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest('dist/'))
 );
 
 gulp.task('photon', () =>
-    gulp.src(__dirname + '/node_modules/photon/**/*', { base: '.' })
-        .pipe(gulp.dest('dist/'))
+    gulp.src(__dirname + '/node_modules/photon/**/*', {
+        base: '.'
+    })
+    .pipe(gulp.dest('dist/'))
 );
 
 gulp.task('runner-files', ['index.html', 'photon']);
@@ -37,10 +39,8 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 
 gulp.task('run', ['runner-files'], () => {
-    const config = Object.assign(
-        {},
-        webpackConfiguration,
-        {
+    const config = Object.assign({},
+        webpackConfiguration, {
             output: {
                 filename: 'app.dev.js',
                 publicPath: 'dist',
